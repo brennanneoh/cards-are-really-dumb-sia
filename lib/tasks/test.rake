2c4dd266300f6c872e46838eb21e2fca358bb05a
Brennan Neoh <brennanneoh@users.noreply.github.com>
2016-05-08 00:26:23 +0800

    Configured color and documentation format for rspec

--- lib/tasks/test.rake
+++ lib/tasks/test.rake
@@ -2,6 +2,10 @@ Rake::Task['test:run'].clear
 
 namespace :test do
 
+  RSpec::Core::RakeTask.new(:spec) do |t|
+    t.fail_on_error = false
+  end
+
   task :run => ['spec', 'spec:javascript']
 
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,6 +55,9 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryGirl::Syntax::Methods
+  config.color = true
+  config.tty = true
+  config.formatter = :documentation
 end
 
 Shoulda::Matchers.configure do |config|
