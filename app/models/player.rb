d81354edc2a0e8b38b6137bf29bc1e681db3dc7f
Brennan Neoh <brennanneoh@users.noreply.github.com>
2016-12-25 00:57:27 +0800

    Remove has_secure_password in player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,9 +1,7 @@
 class Player < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_secure_password
+
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, confirmation: true
