5f75e6fc3021d6d5fad9e936b5ae08dea0c28120
Brennan Neoh <brennanneoh@users.noreply.github.com>
2016-12-25 17:35:54 +0800

    Added game and player model spec

---
+++ spec/models/game_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Game, type: :model do
+  it { is_expected.to have_many(:rounds) }
+  it { is_expected.to have_many(:scores) }
+  it { is_expected.to have_many(:players).through(:scores) }
+end
