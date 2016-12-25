ab1c8b0f8732a654610c0d5a70cd46711806351f
Brennan Neoh <brennanneoh@users.noreply.github.com>
2016-12-25 20:14:22 +0800

    Added name field to devise registration, new and show views in games controller

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,9 @@
+class GamesController < ApplicationController
+  before_action :authenticate_player!
+
+  def create
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :players
-  resources :players
+  devise_for :players, :controllers => { registrations: 'registrations' }
+  get 'signup', to: 'players#sign_up'
+  resources :games, only: [:new, :create, :show]
 end
