3086d1e20c3e9f722a0dda6e6204119a7837a7b5
Brennan Neoh <brennanneoh@users.noreply.github.com>
2016-12-30 08:16:10 +0800

    Refactored cards factory

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -2,21 +2,16 @@ FactoryGirl.define do
   factory :card do
     text { Faker::ChuckNorris.unique.fact }
     stack { create :stack }
-    card_type { Card::TYPES[:black] }
   end
 
-  factory :black_card, class: Card::Black do
-    text { Faker::ChuckNorris.unique.fact }
-    stack { create :stack }
+  factory :black_card, parent: :card, class: Card::Black do
     card_type { Card::TYPES[:black] }
     text_type { [ Card::TEXT_TYPES[:question], Card::TEXT_TYPES[:fill_in_the_blanks] ].sample }
     blanks { 1 }
   end
 
-  factory :white_card, class: Card::White do
+  factory :white_card, parent: :card, class: Card::White do
     text { Faker::TwinPeaks.unique.quote }
-    stack { create :stack }
     text_type { Card::TEXT_TYPES[:answer] }
-    blanks { 0 }
   end
 end
