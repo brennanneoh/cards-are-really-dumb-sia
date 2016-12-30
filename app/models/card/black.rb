31b57caf5d93bbb63b5cfef1e3d96e85c928eb81
Brennan Neoh <brennanneoh@users.noreply.github.com>
2016-12-30 09:11:54 +0800

    Refactoring black card

--- app/models/card/black.rb
+++ app/models/card/black.rb
@@ -3,13 +3,15 @@ class Card::Black < Card
 
   default_scope { where card_type: TYPES[:black] }
 
-  TEXT_TYPES = {
-    question: 'question',
-    fill_in_the_blanks: 'fill_in_the_blanks'
-  }
+  CARD_TYPE = Card::TYPES[:black]
 
-  validates :card_type, presence: true, inclusion: { in: [ Card::TYPES[:black] ] }
-  validates :text_type, presence: true, inclusion: { in: TEXT_TYPES.values }
+  CARD_TEXT_TYPES = [
+    Card::TEXT_TYPES[:question],
+    Card::TEXT_TYPES[:fill_in_the_blanks]
+  ]
+
+  validates :card_type, presence: true, inclusion: { in: [ CARD_TYPE ] }
+  validates :text_type, presence: true, inclusion: { in: CARD_TEXT_TYPES }
   validates :blanks, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }, if: :is_fill_in_the_blanks?
 
   def self.pick_one
@@ -17,27 +19,27 @@ class Card::Black < Card
   end
 
   def text_type_enum
-    TEXT_TYPES.values
+    CARD_TEXT_TYPES
   end
 
   def before_import_save record
-    self.card_type = TYPES[:black]
-    self.text_type = TEXT_TYPES[:fill_in_the_blanks] unless record[:text_type].present?
+    self.card_type = self.CARD_TYPE
+    self.text_type = Card::TEXT_TYPES[:fill_in_the_blanks] unless record[:text_type].present?
     self.stack = Stack.where(name: "Cards Against Humanity").first
     self.count_and_record_blanks
   end
 
   def count_and_record_blanks
-    if self.text_type == TEXT_TYPES[:fill_in_the_blanks]
-      self.blanks = self.text.count '_'
+    self.blanks = if self.text_type == Card::TEXT_TYPES[:fill_in_the_blanks]
+      self.text.count '_'
     else
-      self.blanks = 0
+      1
     end
   end
 
   private
 
   def is_fill_in_the_blanks?
-    self.text_type == TEXT_TYPES[:fill_in_the_blanks]
+    self.text_type == Card::TEXT_TYPES[:fill_in_the_blanks]
   end
 end
